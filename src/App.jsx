b0b369aa711adb42adec7cfed9b024eba389a6c4
sevakul16 <sevakul16@gmail.com>
2024-07-22 14:06:12 +0300

    Result table

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Calculator from "./components/Calculator";
 import Header from "./components/Header";
+import ResultTable from "./components/ResultTable";
 
 function App() {
   return (
     <>
       <Header />
       <Calculator />
+      <ResultTable />
     </>
   );
 }
---
+++ src/components/ResultTable.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ResultTable = () => {
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Investment (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  );
+};
+
+export default ResultTable;
