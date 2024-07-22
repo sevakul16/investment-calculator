a23af360e33267d2286acf1d9ce24d5beaa03eb8
sevakul16 <sevakul16@gmail.com>
2024-07-22 13:47:28 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div id="header">
+      <img
+        src="src\assets\investment-calculator-logo.png"
+        alt="Ironhack logo"
+      />
+      <h1>Investment Calculator</h1>
+    </div>
+  );
+};
+
+export default Header;
